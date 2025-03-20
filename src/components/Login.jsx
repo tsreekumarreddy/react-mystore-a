98442284372856e71c8622e0619e08d48fc67c21
tsreekumarreddy <138125465+tsreekumarreddy@users.noreply.github.com>
2025-03-20 13:57:51 +0530

    new

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <h3>Login Form</h3>
+      <p><input type="text" placeholder='Email address'></input></p>
+      <p><input type="password" placeholder='Password'></input></p>
+      <p><button>Log In</button></p>
+      <p><Link to="../register">Create Account</Link></p>
+    </div>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Register() {
   return (
-    <div>Register</div>
+    <div>
+      <h3>Registration Form</h3>
+      <p><input type="text" placeholder='Enter Name'></input></p>
+      <p><input type="text" placeholder='Email address'></input></p>
+      <p><input type="password" placeholder='New password'></input></p>
+      <p><button>Submit</button></p>
+      <p><Link to="../login">Already a member? Login here!</Link></p>
+    </div>
   )
 }
