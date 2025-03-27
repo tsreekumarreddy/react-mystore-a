9bd28a88ff356cfff387b6bad2fa5628d16b7d82
tsreekumarreddy <138125465+tsreekumarreddy@users.noreply.github.com>
2025-03-27 10:48:00 +0530

    new

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,53 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Register.css";
-
 export default function Register() {
   const [users, setUsers] = useState([]);
-  const [user, setUser] = useState([]);
-
+  const [user, setUser] = useState({});
+  const [msg, setMsg] = useState();
   const handleSubmit = () => {
-    const chk = users.find((item) => item.email === user.email);
-    if (chk) {
-      alert("Email exists. Try with another email.");
-      return;
+    const found = users.find((value) => value.email === user.email);
+    if (found) {
+      setMsg("User already exists");
+    } else {
+      setMsg();
+      setUsers([...users, user]);
+      setUser({ ...user, name: "", email: "", password: "" });
     }
-    
-    setUsers([...users, user]);
-    // setUser({ name: "", email: "", password: "" }); 
   };
-
   const handleDelete = (email) => {
-    setUsers(users.filter((value) => value.email !== email));
+    setUsers(users.filter((value) => value.email != email));
   };
-
   return (
     <div className="App-Register-Row">
       <div className="App-Register-Box">
         <h3>Registration Form</h3>
+        {msg}
         <p>
           <input
             type="text"
-            placeholder="Enter Name"
             value={user.name}
+            placeholder="Enter Name"
             onChange={(e) => setUser({ ...user, name: e.target.value })}
-          />
+          ></input>
         </p>
         <p>
           <input
             type="text"
-            placeholder="Email address"
             value={user.email}
+            placeholder="Email address"
             onChange={(e) => setUser({ ...user, email: e.target.value })}
-          />
+          ></input>
         </p>
         <p>
           <input
             type="password"
-            placeholder="New password"
             value={user.password}
+            placeholder="New password"
             onChange={(e) => setUser({ ...user, password: e.target.value })}
-          />
+          ></input>
         </p>
         <p>
           <button onClick={handleSubmit}>Submit</button>
@@ -56,29 +55,27 @@ export default function Register() {
           <Link to="../login">Already a member? Login here!</Link>
         </p>
       </div>
-
       <div className="App-Register-Box">
         <h3>Userlist</h3>
         <table className="App-Register-Table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Password</th>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th></th>
+          </tr>
+          {users.map((value, index) => (
+            <tr key={index}>
+              <td>{value.name}</td>
+              <td>{value.email}</td>
+              <td>{value.password}</td>
+              <td>
+                <button onClick={() => handleDelete(value.email)}>
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {users.map((value, index) => (
-              <tr key={index}>
-                <td>{value.name}</td>
-                <td>{value.email}</td>
-                <td>{value.password}</td>
-                <td>
-                  <button onClick={() => handleDelete(value.email)}>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          ))}
         </table>
       </div>
     </div>
